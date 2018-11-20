6ecef38e7ec34e43ab56de8e0937958710b5be97
Ami Desai <ami.desai@infostretch.com>
2018-11-20 12:52:05 +0530

    deployment script added

---
+++ vars/nativDeploy.groovy
@@ -0,0 +1,8 @@
+#!/usr/bin/env groovy
+
+def call(){
+	withCredentials([sshUserPrivateKey(credentialsId: "c09ec154-b893-4588-a5ab-919c75fc6808", keyFileVariable: 'KEY_FILE')]) {
+            echo '$KEY_FILE'
+            sh "scp -i ${KEY_FILE} $WORKSPACE/package/KRelease.ipa kavit.patel@10.12.46.130:/Users/kavit.patel/Desktop"
+        }
+}
